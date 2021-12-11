cb4ed4bddbb1ebf1bac847ca3df278382cb42b33
vishnu1717 <95946234+vishnu1717@users.noreply.github.com>
2021-12-10 21:25:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-'''Train CIFAR10 with PyTorch.'''
+'''Train CIFAR100 with PyTorch.'''
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -15,7 +15,7 @@ from models import *
 from utils import progress_bar
 
 
-parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
+parser = argparse.ArgumentParser(description='PyTorch CIFAR100 Training')
 parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
 parser.add_argument('--resume', '-r', action='store_true',
                     help='resume from checkpoint')
@@ -39,36 +39,41 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(
+trainset = torchvision.datasets.CIFAR100(
     root='./data', train=True, download=True, transform=transform_train)
 trainloader = torch.utils.data.DataLoader(
     trainset, batch_size=128, shuffle=True, num_workers=2)
 
-testset = torchvision.datasets.CIFAR10(
+testset = torchvision.datasets.CIFAR100(
     root='./data', train=False, download=True, transform=transform_test)
 testloader = torch.utils.data.DataLoader(
     testset, batch_size=100, shuffle=False, num_workers=2)
 
 classes = ('plane', 'car', 'bird', 'cat', 'deer',
-           'dog', 'frog', 'horse', 'ship', 'truck')
+           'dog', 'frog', 'horse', 'ship', 'truck','beaver', 'dolphin', 'otter', 'seal', 'whale'
+	'aquarium fish', 'flatfish', 'ray, shark', 'trout'
+	'orchids', 'poppies', 'roses', 'sunflowers', 'tulips'
+	'bottles', 'bowls', 'cans', 'cups', 'plates'
+ 	'apples', 'mushrooms', 'oranges', 'pears', 'sweet peppers'
+	'clock', 'computer keyboard', 'lamp', 'telephone', 'television'
+	'bed', 'chair', 'couch', 'table', 'wardrobe'
+	'bee', 'beetle', 'butterfly', 'caterpillar', 'cockroach'
+	'bear', 'leopard', 'lion', 'tiger', 'wolf'
+	'bridge', 'castle', 'house', 'road', 'skyscraper'
+	'cloud', 'forest', 'mountain', 'plain', 'sea'
+	'camel', 'cattle', 'chimpanzee', 'elephant', 'kangaroo'
+	'fox', 'porcupine', 'possum', 'raccoon', 'skunk'
+	'crab', 'lobster', 'snail', 'spider', 'worm'
+	'baby', 'boy', 'girl', 'man', 'woman'
+	'crocodile', 'dinosaur', 'lizard', 'snake', 'turtle'
+	'hamster', 'mouse', 'rabbit', 'shrew', 'squirrel'
+	'maple', 'oak', 'palm', 'pine', 'willow'
+	'bicycle', 'bus', 'motorcycle', 'pickup truck', 'train'
+	'lawn-mower', 'rocket', 'streetcar', 'tank', 'tractor')
 
 # Model
 print('==> Building model..')
-# net = VGG('VGG19')
-# net = ResNet18()
-# net = PreActResNet18()
-# net = GoogLeNet()
-# net = DenseNet121()
-# net = ResNeXt29_2x64d()
-# net = MobileNet()
-# net = MobileNetV2()
-# net = DPN92()
-# net = ShuffleNetG2()
-# net = SENet18()
-# net = ShuffleNetV2(1)
-# net = EfficientNetB0()
-# net = RegNetX_200MF()
-net = SimpleDLA()
+net = ResNet18()
 net = net.to(device)
 if device == 'cuda':
     net = torch.nn.DataParallel(net)
